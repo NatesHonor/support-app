30a24d65ca68b0726c152347df7a84065d7a31fd
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-05 20:54:34 -0400

    Updated tickets page

--- src/pages/TicketsPage.js
+++ src/pages/TicketsPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { Container, Typography, Button, Box, Grid, Card, CardContent } from '@mui/material';
+import { Container, Typography, Button, Box, Grid, Card, CardContent, AppBar, Toolbar, IconButton } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
@@ -11,7 +13,7 @@ const TicketsPage = () => {
   useEffect(() => {
     const user = Cookies.get('username');
     if (!user) {
-      window.location.href = 'https://www.natemarcellus.com/login'; // Redirect to login page
+      window.location.href = 'https://www.natemarcellus.com/login';
     } else {
       setUsername(user);
     }
@@ -23,6 +25,16 @@ const TicketsPage = () => {
 
   return (
     <ThemeProvider theme={darkTheme}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Ticket Management
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Container component="main" maxWidth="lg" sx={{ marginTop: 8 }}>
         <Typography variant="h4" component="h1" align="center" gutterBottom>
           Tickets
@@ -31,7 +43,7 @@ const TicketsPage = () => {
           Welcome, {username}! Here you can manage your tickets.
         </Typography>
         <Box display="flex" justifyContent="flex-end" mb={2}>
-          <Button variant="contained" color="primary" onClick={() => alert('Create New Ticket Logic Here!')}>
+          <Button variant="contained" color="primary" startIcon={<AddCircleOutlineIcon />} onClick={() => alert('Create New Ticket Logic Here!')}>
             Create New Ticket
           </Button>
         </Box>
@@ -49,6 +61,11 @@ const TicketsPage = () => {
           ))}
         </Grid>
       </Container>
+      <Box mt={5} py={3} textAlign="center" bgcolor="background.paper">
+        <Typography variant="body2" color="textSecondary">
+          © 2024 Your Company. All rights reserved.
+        </Typography>
+      </Box>
     </ThemeProvider>
   );
 };
