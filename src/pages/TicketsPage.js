e474ad2b060799ec3bbfa8d9c60b5d4e7c126535
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-05 22:18:36 -0400

    updated dotenv

--- src/pages/TicketsPage.js
+++ src/pages/TicketsPage.js
@@ -7,6 +7,7 @@ import {
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+require('dotenv').config();
 
 const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
