5747d9b6a910f75a67e783b3dae768b16ff0a5f4
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-06 14:24:23 -0400

    Removed use callbnack

--- src/pages/TicketsPage.js
+++ src/pages/TicketsPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import { 
   Container, Typography, Button, Box, Grid, Card, CardContent, AppBar, 
